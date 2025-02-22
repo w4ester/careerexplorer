8dc06e18ad1f4167d4ff9c18ab9ad5005f6cc762
Will F <11763006+w4ester@users.noreply.github.com>
2025-02-21 22:09:33 -0500

    Add UI components

---
+++ src/components/ui/alert.jsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+const Alert = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(
+      "relative w-full rounded-lg border p-4",
+      className
+    )}
+    {...props}
+  />
+))
+Alert.displayName = "Alert"
+
+const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
+
+const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
